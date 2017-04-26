f517159dece90d890aed8a727cdba4e42a0a3ac3
Ahmed <ahmed.hussein.safa@gmail.com>
2017-04-26 20:19:16 +0000

    add validation

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,12 @@
 class Patient < ActiveRecord::Base
   belongs_to :doctor
+validates_presence_of :first_name, message: 'please fill the blank'
+validates_presence_of :last_name, message: 'please fill the blank'
+validates_uniqueness_of :first_name
+validates_uniqueness_of :last_name
+validates_uniqueness_of :phone
+validates_format_of :phone, with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+validates_uniqueness_of :email
+validates_format_of :email, :with => /\A([^@\s]+)@((gmail)\.+com)\Z/i, :on => :create ,:message =>"must be @gmail.com"
+
 end
